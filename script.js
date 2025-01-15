e410d86708c11d04ae872e71de36ec99c17e0325
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-15 23:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let select=document.getElementById('colorSelect');
+let btn=document.querySelector('input[type="button"]');
+btn.addEventListener('click',()=>{
+let options=document.querySelectorAll('option');
+	for(let el of options){
+		if(el.value==select.value){
+			select.removeChild(el);
+			break;
+		}
+	}
+
+  
+})
